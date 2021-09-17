87f4a574f40bfc4246e00fedf5cd9c08f39e9fe1
maxekspb <dmaxek@gmail.com>
2021-09-18 01:05:26 +0300

    0 lab

---
+++ lab 0/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int x = 0;
+    scanf ("%d", &a);
+    scanf ("%d", &b);
+    scanf ("%d", &c);
+    x = b - c;
+    printf("%d \n", x);
+    
+    return 0;
+}
