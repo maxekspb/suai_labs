36236a29f154276ca2d38d3b68f4bd84cc007409
maxekspb <dmaxek@gmail.com>
2021-09-25 00:14:42 +0300

    Lab 1

---
+++ lab1/ex5.c
@@ -0,0 +1,22 @@
+// Ввести действительное число x, вычислить и вывести y = x^10 + 2 * x^9 + 3 * x^8 + ... + 10 * x + 11.
+
+#include <stdio.h>
+
+int main() {
+    int x, y, a, res, st, i;
+    scanf("%d", &x);
+    y = 0;
+    a = 1;
+    res = 0;
+        while(y <= 10) {
+        st = 1;
+            for(i = y; i < 10; i++) {
+                st = st * x;
+            }
+        y++;
+        res = res + (st * a);
+        a++;
+        }
+    printf("%d", res);
+    return 0;
+}
